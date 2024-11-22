4eb5f3fab9a0e9c8e838fbff86edce7a8a6a6ba0
Matěj Minařík <majsek555@gmail.com>
2024-11-22 21:47:20 +0100

    FetchPokemon raw

--- Program.cs
+++ Program.cs
@@ -10,13 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // Registrace HttpClient služby
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-// // Nastavení CORS (pokud je potřeba na serverové straně)
-// builder.Services.AddCors(options =>
-// {
-//     options.AddPolicy("AllowLocalhost",
-//         policy => policy.WithOrigins("http://localhost:8081") // Nastav správnou URL pro tvoji aplikaci
-//                         .AllowAnyMethod()
-//                         .AllowAnyHeader());
-// });
 
 await builder.Build().RunAsync();
