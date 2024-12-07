fcef7a1803fb6e6b25570ca7118f539ef3cf7faf
Matěj Minařík <majsek555@gmail.com>
2024-12-07 02:22:11 +0100

    +UserInfo model

---
+++ Shared/UserInfo.cs
@@ -0,0 +1,21 @@
+using System.Text.Json.Serialization;
+
+namespace PokedexGambaApp.Shared;
+
+public class UserInfo
+{
+    [JsonPropertyName("id")]
+    public required string Id { get; set; }
+    [JsonPropertyName("username")]
+    public required string Username { get; set; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("surname")]
+    public required string Surname { get; set; }
+
+    [JsonPropertyName("email")]
+    public required string Email { get; set; }
+}
+
